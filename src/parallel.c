ca7e5f7b27fe84b2c4fb4a4cb053c9c9b66c0ba7
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-07-01 11:31:17 +0200

    fix(parallel): k, N, clock without print time

--- src/parallel.c
+++ src/parallel.c
@@ -1,95 +1,94 @@
-#include <math.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
 #include <time.h>
 
-#define MAX_THREADS 7
+#define k 7
+#define N 20
 
 typedef struct {
-  int n;
-  int* A;
-  int start;
-  int end;
+    int n;
+    int* A;
+    int start;
+    int end;
 } ThreadArgs;
 
 void* sieveOfEratosthenes(void* arg) {
-  ThreadArgs* threadArgs = (ThreadArgs*)arg;
-  int n = threadArgs->n;
-  int* A = threadArgs->A;
-  int start = threadArgs->start;
-  int end = threadArgs->end;
+    ThreadArgs* threadArgs = (ThreadArgs*)arg;
+    int n = threadArgs->n;
+    int* A = threadArgs->A;
+    int start = threadArgs->start;
+    int end = threadArgs->end;
 
-  for (int i = 2; i * i < n; i++) {
-    if (A[i] == 1) {
-      for (int j = i * i; j < n; j += i) {
-        if (j >= start && j < end) {
-          A[j] = 0;
+    for (int i = 2; i * i < n; i++) {
+        if (A[i] == 1) {
+            for (int j = i * i; j < n; j += i) {
+                    if (j >= start && j < end) {
+                        A[j] = 0;
+                    }
+            }
         }
-      }
     }
-  }
 
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 int main() {
-  clock_t start, end;
-  double cpu_time_used;
-  // Début du chronomètre
-  start = clock();
+    clock_t start, end;
+    double cpu_time_used;
+    // Début du chronomètre
+    start = clock();
 
-  int n = 4000000;
-  int* A = malloc(sizeof(int) * n);
-  for (int i = 2; i < n; i++) {
-    A[i] = 1;
-  }
-
-  int numThreads = MAX_THREADS;  // Nombre de threads à utiliser
-  pthread_t threads[MAX_THREADS];
-  ThreadArgs threadArgs[MAX_THREADS];
-
-  int range =
-      (n - 2) / numThreads;  // Taille de la plage gérée par chaque thread
-
-  for (int i = 0; i < numThreads; i++) {
-    threadArgs[i].n = n;
-    threadArgs[i].A = A;
-    threadArgs[i].start = 2 + i * range;
-    threadArgs[i].end = (i == numThreads - 1) ? n : (2 + (i + 1) * range);
-
-    int result = pthread_create(&threads[i], NULL, sieveOfEratosthenes,
-                                (void*)&threadArgs[i]);
-    if (result != 0) {
-      printf("Erreur lors de la création du thread.\n");
-      return 1;
+    int n = N;
+    int* A = malloc(sizeof(int) * n);
+    for (int i = 2; i < n; i++) {
+        A[i] = 1;
     }
-  }
 
-  for (int i = 0; i < numThreads; i++) {
-    int result = pthread_join(threads[i], NULL);
-    if (result != 0) {
-      printf("Erreur lors de l'attente du thread.\n");
-      return 1;
+    int numThreads = k; // Nombre de threads à utiliser
+    pthread_t threads[k];
+    ThreadArgs threadArgs[k];
+
+    int range = (n - 2) / numThreads; // Taille de la plage gérée par chaque thread
+
+    for (int i = 0; i < numThreads; i++) {
+        threadArgs[i].n = n;
+        threadArgs[i].A = A;
+        threadArgs[i].start = 2 + i * range;
+        threadArgs[i].end = (i == numThreads - 1) ? n : (2 + (i + 1) * range);
+
+        int result = pthread_create(&threads[i], NULL, sieveOfEratosthenes, (void*)&threadArgs[i]);
+        if (result != 0) {
+            printf("Erreur lors de la création du thread.\n");
+            return 1;
+        }
     }
-  }
 
-  printf("Les nombres premiers sont :\n");
-  for (int i = 2; i < n; i++) {
-    if (A[i] == 1) {
-      printf("%d\n", i);
+    for (int i = 0; i < numThreads; i++) {
+        int result = pthread_join(threads[i], NULL);
+        if (result != 0) {
+            printf("Erreur lors de l'attente du thread.\n");
+            return 1;
+        }
     }
-  }
 
-  free(A);
+    // Fin du chronomètre
+    end = clock();
 
-  // Fin du chronomètre
-  end = clock();
+    // Calcul du temps écoulé en secondes
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-  // Calcul du temps écoulé en secondes
-  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Les nombres premiers sont :\n");
+    for (int i = 2; i < n; i++) {
+        if (A[i] == 1) {
+            printf("%d\n", i);
+        }
+    }
+    
+    free(A);
 
-  // Affichage du temps d'exécution
-  printf("Temps d'exécution : %f secondes\n", cpu_time_used);
-  return 0;
+    // Affichage du temps d'exécution
+    printf("Temps d'exécution : %f secondes\n", cpu_time_used);
+    return 0;
 }
